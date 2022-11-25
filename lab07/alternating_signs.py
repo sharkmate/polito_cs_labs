c738bdb4d4c529b1d647c3accfdbe7f0e0391d53
sharkmate <sofia.alvise03@gmail.com>
2022-11-25 11:59:48 +0100

    added lab07

---
+++ lab07/alternating_signs.py
@@ -0,0 +1,27 @@
+"""
+Sum with alternating signs.
+Write a program that receives as input a sequence of integers (terminated by an empty line),
+and that calculates the alternating sum of its elements.
+For example, if the program reads the data 1 4 9 16 9 7 4 9 11,
+it must calculate and display 1 - 4 + 9 - 16 + 9 - 7 + 4 - 9 + 11 = –2.
+"""
+
+
+def main():
+    nums = 0
+    nums_list = []
+    result = 0
+    while not nums == " ":
+        nums = (input("insert a number, stop with a white space "))
+        nums_list.append(nums)
+    nums_list.pop(-1)
+    for i in nums_list[1::2]:
+        result += int(i)
+    for j in nums_list[0::2]:
+        result -= int(j)
+
+    print(nums_list, result)
+
+
+if __name__ == "__main__":
+    main()
